14eec2370a986d543f744032718c0e68ae5f3f08
nowang6 <norman6.wang@gmail.com>
2025-10-21 11:54:12 +0800

    test

--- td-test/agent_precise.py
+++ td-test/agent_precise.py
@@ -1,12 +1,10 @@
 from evalscope.run import run_task
-from evalscope.config import TaskConfig
 
-
-task_cfg = TaskConfig(
-    model='Qwen3-30B-A3B-Instruct-2507',
-    datasets=['gsm8k', 'arc'],
-    api-url='http://127.0.0.1:1025/v1/chat/completions',
-    api-key='empty',
-    limit=5
-)
+task_cfg = {
+    "model": "Qwen3-30B-A3B-Instruct-2507",
+    "datasets": ["gsm8k", "arc"],
+    "api_url": "http://127.0.0.1:1025/v1/chat/completions",
+    "api_key": "empty",
+    "limit": 5
+}
 run_task(task_cfg=task_cfg)
